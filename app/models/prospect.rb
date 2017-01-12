c7b85fc394aaf9f3fdbd8ba45ed7b85667548ae8
John Bard <john.bard@tuskerdirect.com>
2017-01-12 10:50:15 +0000

    Next package to be received

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,3 +1,7 @@
 class Prospect < ActiveResource::Base
   self.site = 'http://tuskermarvel.com/'
+
+  def next_package
+    Package.all.detect{ |package| (package.contents & self.received).empty? }
+  end
 end
--- spec/models/prospect_spec.rb
+++ spec/models/prospect_spec.rb
@@ -14,4 +14,31 @@ RSpec.describe Prospect, :type => :model do
     subject { Prospect.all }
     it { is_expected.to all(be_a(Prospect)) }
   end
+
+  describe 'next_package' do
+    before { ActiveResource::HttpMock.respond_to { |mock| mock.get "/prospects.json", {}, prospects } }
+    subject { Prospect.all.detect{ |p| p.contact == 'Alice' }.next_package }
+
+    context 'when package exists where content not already received' do
+      let(:package1) { Package.new(code: 'pk1', contents: [ '1a', '2b' ]) }
+      let(:package2) { Package.new(code: 'pk2', contents: [ '2b', '3a' ]) }
+      let(:package3) { Package.new(code: 'pk3', contents: [ '3a', '1b' ]) }
+      let(:packages) { [ package1, package2, package3 ] }
+      it 'returns package with unreceived content' do
+        expect(Package).to receive(:all).and_return(packages)
+        is_expected.to eq package2
+      end
+    end
+
+    context 'when no package exists where content not already received' do
+      let(:package1) { Package.new(code: 'pk1', contents: [ '1a', '2b' ]) }
+      let(:package2) { Package.new(code: 'pk2', contents: [ '2b', '1a' ]) }
+      let(:package3) { Package.new(code: 'pk3', contents: [ '3a', '1b' ]) }
+      let(:packages) { [ package1, package2, package3 ] }
+      it 'returns nil' do
+        expect(Package).to receive(:all).and_return(packages)
+        is_expected.to be_nil
+      end
+    end
+  end
 end
